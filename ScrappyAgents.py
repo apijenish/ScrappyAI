c11f32f64f206ad42baee70bdaea511c0f4d7558
Jenish Chand <jenish.chand@students.kennesaw.edu>
2026-02-15 20:04:28 -0500

    agents

--- ScrappyAgents.py
+++ ScrappyAgents.py
@@ -8,4 +8,16 @@ class InvestigationAgents:
     def intent_agent(self, state:InvestigationState)-> InvestigationState:
         pass
 
+    def planner_agent(self, state:InvestigationState)->InvestigationState:
+        pass
+
+    def query_builder_agent(self, state:InvestigationState)->InvestigationState:
+        pass
+
+    def data_retrieval_agent(self, state:InvestigationState)->InvestigationState:
+        pass
+
+    def summary_agent(self, state:InvestigationState)->InvestigationState:
+        pass
+
 
